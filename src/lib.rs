f7b9ba03fba59eb57eda65eb91759cc4bee66c2d
Nikita Nazarenko <savant.d@gmail.com>
2015-05-25 10:28:01 +0300

    transitional commit. will be rebased

--- src/lib.rs
+++ src/lib.rs
@@ -5,31 +5,45 @@ mod xdr {
 	use std::io::{BufReader,BufWriter,Cursor};
 	use std::str;
 	use std::vec::Vec;
-	use byteorder::{BigEndian,ReadBytesExt,WriteBytesExt};
-	static pad_len:u32 = 4;
+	use byteorder::{BigEndian,ReadBytesExt,WriteBytesExt,Error};
+	static PAD_LEN:u32 = 4;
 
-	pub enum Error {
-		UnexpectedValue,
-	}
 	pub struct XdrReader<'a> {
 		reader : BufReader<&'a [u8]>
 	}
 
 	pub struct XdrWriter<'a> {
-		writer : BufWriter<&'a mut [u8]>
+		writer : Box<BufWriter<&'a mut Vec<u8>>>
 	}
 
+	impl<'a> XdrWriter<'a> {
+		fn new() -> XdrWriter<'a> {
+			let v = Box::new(Vec::new());
+			XdrWriter{ writer: BufWriter::new(&mut v)}
+		}
+	}	
+
 	pub trait XdrPrimitive {
-		fn read_from_xdr(&x: &mut XdrReader, _ : Option<Self>) -> Result<Self,Error>;
-		fn write_to_xdr(&x: &mut XdrWriter, v: Option<Self>);
+		fn read_from_xdr(x: &mut XdrReader) -> Result<Self, Error>;
+		fn write_to_xdr(x: &mut XdrWriter, v: Self);
 	}
 
 	impl XdrPrimitive for u32 {
-		fn read_from_xdr(&x: &mut XdrReader, _ : Option<u32>) -> Result<u32, Error>{
-			x.read_u32::<BigEndian>().unwrap();	
+		fn read_from_xdr(x: &mut XdrReader) -> Result<u32, Error>{
+			match x.reader.read_u32::<BigEndian>() {
+				Ok(v) => Ok(v),
+				Err(v) => Err(v)
+			}
+				
 		}
-		fn write_to_xdr(&x: &mut XdrWriter, v:Option<Self>) {
-			x.write_u32::<BigEndian>(Some(v)).unwrap();
+		fn write_to_xdr(x: &mut XdrWriter, v:Self) {
+			x.writer.unwrap().write_u32::<BigEndian>(v).unwrap();
 		}
 	}
+}
+
+
+#[test]
+fn u32_test() {
+	assert!(false)
 }
