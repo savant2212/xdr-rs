1af2d3327fbe1060b08d8f4d08d0df39aafcb1ff
Nikita Nazarenko <savant.d@gmail.com>
2015-05-29 01:28:08 +0300

    move xdr to separate file, write imps for baisc types

--- src/lib.rs
+++ src/lib.rs
@@ -1,66 +1,10 @@
-#![crate_id="xdr#0.0.1"]
+#![crate_id="xdr#1.0.1"]
 #![crate_type="lib"]
 extern crate byteorder;
-mod xdr {
-	use std::io::{BufReader,BufWriter,Cursor};
-	use std::str;
-	use std::vec::Vec;
-	use byteorder::{BigEndian,ReadBytesExt,WriteBytesExt,Error};
-	static PAD_LEN:u32 = 4;
-
-	pub struct XdrReader<'a> {
-		reader : BufReader<&'a [u8]>
-	}
-
-	pub struct XdrWriter {
-		writer : Cursor<Vec<u8>>
-	}
-
-	impl XdrWriter {
-		pub fn new() -> XdrWriter {
-			let v : Vec<u8>= Vec::new();
-			XdrWriter{ writer: Cursor::new(v)}
-		}
-
-		pub fn get_data(self) -> Vec<u8> {
-			self.writer.into_inner()
-		}
-			
-	}	
-	pub trait XdrPrimitive {
-		fn read_from_xdr(x: &mut XdrReader) -> Result<Self, Error>;
-		fn write_to_xdr(x: &mut XdrWriter, v: Self);
-	}
-
-	impl XdrPrimitive for u32 {
-		fn read_from_xdr(x: &mut XdrReader) -> Result<u32, Error>{
-			match x.reader.read_u32::<BigEndian>() {
-				Ok(v) => Ok(v),
-				Err(v) => Err(v)
-			}
-				
-		}
-		fn write_to_xdr(x: &mut XdrWriter, v:Self) {
-			x.writer.write_u32::<BigEndian>(v).unwrap();
-		}
-	}
-	impl XdrPrimitive for u16 {
-		fn read_from_xdr(x: &mut XdrReader) -> Result<u16, Error>{
-			match x.reader.read_u16::<BigEndian>() {
-				Ok(v) => Ok(v),
-				Err(v) => Err(v)
-			}
-				
-		}
-		fn write_to_xdr(x: &mut XdrWriter, v:Self) {
-			x.writer.write_u16::<BigEndian>(v).unwrap();
-		}
-	}
-}
-
+mod xdr;
 
 #[test]
-fn u32_test() {
+fn u16_writer_test() {
 	use std::io::{BufReader,BufWriter,Cursor};
 	use std::str;
 	use std::vec::Vec;
