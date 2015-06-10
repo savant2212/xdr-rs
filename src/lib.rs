257f7001bbb1ecc02119f99c56753f6815b2be7c
Nikita Nazarenko <nnazarenko@radiofid.ru>
2015-06-10 16:40:47 +0300

    cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,12 @@
-#![crate_id="xdr#1.0.1"]
 #![crate_type="lib"]
 extern crate byteorder;
 pub mod xdr;
 
 #[test]
 fn u16_writer_test() {
-	use std::io::{BufReader,BufWriter,Cursor};
-	use std::str;
-	use std::vec::Vec;
-	use byteorder::{BigEndian,ReadBytesExt,WriteBytesExt,Error};
-
 	let mut x = xdr::XdrWriter::new();
-	let mut wtr = vec![];
+	let wtr = vec![0,0,2,5];
 
-	wtr.write_u16::<BigEndian>(517).unwrap();
-	xdr::XdrPrimitive::write_to_xdr(&mut x,517u16);
+	x.pack(517u16);
 	assert_eq!(x.get_buffer(),wtr);
 }
