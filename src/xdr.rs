1fbfefc7c0ea9bb88b4b80b44a87a9a4ed10d3f5
Nikita Nazarenko <nnazarenko@radiofid.ru>
2015-06-10 16:45:48 +0300

    Type cleanups

--- src/xdr.rs
+++ src/xdr.rs
@@ -84,123 +84,124 @@ pub trait XdrPrimitive {
 
 impl XdrPrimitive for u32 {
 	fn read_from_xdr(x: &mut XdrReader) -> Result<u32, Error>{
-		match x.reader.read_u32::<BigEndian>() {
+		match x.reader.read_u32::<byteorder::BigEndian>() {
 			Ok(v) => Ok(v),
-			Err(v) => Err(v)
+			Err(v) => Err(Error::Io(v))
 		}
 			
 	}
 	fn write_to_xdr(x: &mut XdrWriter, v:Self) {
-		x.writer.write_u32::<BigEndian>(v).unwrap();
+		x.writer.write_u32::<byteorder::BigEndian>(v).unwrap();
 	}
 }
 impl XdrPrimitive for u16 {
 	fn read_from_xdr(x: &mut XdrReader) -> Result<u16, Error>{
-		match x.reader.read_u32::<BigEndian>() {
+		match x.reader.read_u32::<byteorder::BigEndian>() {
 			Ok(v) => Ok(v as u16),
-			Err(v) => Err(v)
+			Err(v) => Err(Error::Io(v))
 		}
 			
 	}
 	fn write_to_xdr(x: &mut XdrWriter, v:Self) {
-		x.writer.write_u32::<BigEndian>(v as u32).unwrap();
+		x.writer.write_u32::<byteorder::BigEndian>(v as u32).unwrap();
 	}
 }
 impl XdrPrimitive for u8 {
 	fn read_from_xdr(x: &mut XdrReader) -> Result<u8, Error>{
-		match x.reader.read_u32::<BigEndian>() {
+		match x.reader.read_u32::<byteorder::BigEndian>() {
 			Ok(v) => Ok(v as u8),
-			Err(v) => Err(v)
+			Err(v) => Err(Error::Io(v))
 		}
 			
 	}
 	fn write_to_xdr(x: &mut XdrWriter, v:Self) {
-		x.writer.write_u32::<BigEndian>(v as u32).unwrap();
+		x.writer.write_u32::<byteorder::BigEndian>(v as u32).unwrap();
 	}
 }
 impl XdrPrimitive for i8 {
 	fn read_from_xdr(x: &mut XdrReader) -> Result<i8, Error>{
-		match x.reader.read_i32::<BigEndian>() {
+		match x.reader.read_i32::<byteorder::BigEndian>() {
 			Ok(v) => Ok(v as i8),
-			Err(v) => Err(v)
+			Err(v) => Err(Error::Io(v))
 		}
 			
 	}
 	fn write_to_xdr(x: &mut XdrWriter, v:Self) {
-		x.writer.write_i32::<BigEndian>(v as i32).unwrap();
+		x.writer.write_i32::<byteorder::BigEndian>(v as i32).unwrap();
 	}
 }
 impl XdrPrimitive for i16 {
 	fn read_from_xdr(x: &mut XdrReader) -> Result<i16, Error>{
-		match x.reader.read_i32::<BigEndian>() {
+		match x.reader.read_i32::<byteorder::BigEndian>() {
 			Ok(v) => Ok(v as i16),
-			Err(v) => Err(v)
+			Err(v) => Err(Error::Io(v))
 		}
 			
 	}
 	fn write_to_xdr(x: &mut XdrWriter, v:Self) {
-		x.writer.write_i32::<BigEndian>(v as i32).unwrap();
+		x.writer.write_i32::<byteorder::BigEndian>(v as i32).unwrap();
 	}
 }	
 impl XdrPrimitive for i32 {
 	fn read_from_xdr(x: &mut XdrReader) -> Result<i32, Error>{
-		match x.reader.read_i32::<BigEndian>() {
+		match x.reader.read_i32::<byteorder::BigEndian>() {
 			Ok(v) => Ok(v),
-			Err(v) => Err(v)
+			Err(v) => Err(Error::Io(v))
 		}
 			
 	}
 	fn write_to_xdr(x: &mut XdrWriter, v:Self) {
-		x.writer.write_i32::<BigEndian>(v).unwrap();
+		x.writer.write_i32::<byteorder::BigEndian>(v).unwrap();
 	}
 }
 impl XdrPrimitive for i64 {
 	fn read_from_xdr(x: &mut XdrReader) -> Result<i64, Error>{
-		match x.reader.read_i64::<BigEndian>() {
+		match x.reader.read_i64::<byteorder::BigEndian>() {
 			Ok(v) => Ok(v),
-			Err(v) => Err(v)
+			Err(v) => Err(Error::Io(v))
 		}
 			
 	}
 	fn write_to_xdr(x: &mut XdrWriter, v:Self) {
-		x.writer.write_i64::<BigEndian>(v).unwrap();
+		x.writer.write_i64::<byteorder::BigEndian>(v).unwrap();
 	}
 }
 impl XdrPrimitive for u64 {
 	fn read_from_xdr(x: &mut XdrReader) -> Result<u64, Error>{
-		match x.reader.read_u64::<BigEndian>() {
+		match x.reader.read_u64::<byteorder::BigEndian>() {
 			Ok(v) => Ok(v),
-			Err(v) => Err(v)
+			Err(v) => Err(Error::Io(v))
 		}
 			
 	}
 	fn write_to_xdr(x: &mut XdrWriter, v:Self) {
-		x.writer.write_u64::<BigEndian>(v).unwrap();
+		x.writer.write_u64::<byteorder::BigEndian>(v).unwrap();
 	}
 
 }
 
 impl XdrPrimitive for f32 {
 	fn read_from_xdr(x: &mut XdrReader) -> Result<f32, Error>{
-		match x.reader.read_f32::<BigEndian>() {
+		match x.reader.read_f32::<byteorder::BigEndian>() {
 			Ok(v) => Ok(v),
-			Err(v) => Err(v)
+			Err(v) => Err(Error::Io(v))
 		}
 			
 	}
 	fn write_to_xdr(x: &mut XdrWriter, v:Self) {
-		x.writer.write_f32::<BigEndian>(v).unwrap();
+		x.writer.write_f32::<byteorder::BigEndian>(v).unwrap();
 	}
-}	
+}
 impl XdrPrimitive for f64 {
 	fn read_from_xdr(x: &mut XdrReader) -> Result<f64, Error>{
-		match x.reader.read_f64::<BigEndian>() {
+		match x.reader.read_f64::<byteorder::BigEndian>() {
 			Ok(v) => Ok(v),
-			Err(v) => Err(v)
+			Err(v) => Err(Error::Io(v))
 		}
 			
 	}
 	fn write_to_xdr(x: &mut XdrWriter, v:Self) {
-		x.writer.write_f64::<BigEndian>(v).unwrap();
+		x.writer.write_f64::<byteorder::BigEndian>(v).unwrap();
 	}
-}	
+}
+
